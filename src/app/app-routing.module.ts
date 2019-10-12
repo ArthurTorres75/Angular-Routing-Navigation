d57b208e0e59de6fbba5af279b5399606970d10e
Arthur <arthurtorres75@gmail.com>
2019-10-11 20:13:01 -0400

    hito 4 routes crisis center

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CrisisListComponent } from './crisis-list/crisis-list.component';
+// import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
+  // { path: 'crisis-center', component: CrisisListComponent },
   // { path: 'heroes', component: HeroListComponent},
   { path: '', redirectTo: '/heroes', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
--- src/app/crisis-center/crisis-center-routing.module.ts
+++ src/app/crisis-center/crisis-center-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { CrisisHomeComponent } from './crisis-home/crisis-home.component';
+import { CrisisCenterComponent } from './crisis-center/crisis-center.component';
+import { CrisisDetailComponent } from './crisis-detail/crisis-detail.component';
 
 
-const crisisCenterRoutes: Routes = [];
+const crisisCenterRoutes: Routes = [
+  {
+    path: 'crisis-center',
+    component: CrisisCenterComponent,
+    children: [
+      {
+        path: '', component: CrisisListComponent,
+        children: [
+          {
+            path: ':id', component: CrisisDetailComponent
+          },
+          {
+            path: '', component: CrisisHomeComponent 
+          }
+        ]
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(crisisCenterRoutes)],
